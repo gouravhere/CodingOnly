1abfab1d7a4a5bde6b0a37de55401f22b1728509
gourav sharma <gourav.101sharma@gmail.com>
2025-09-08 23:19:56 +0530

    code

---
+++ src/main/java/DSA/MoveZerosToEnd.java
@@ -0,0 +1,25 @@
+package DSA;
+
+import java.util.Arrays;
+
+/*
+Problem Statement
+
+“Given an array, move all zeros to the end while keeping the relative order of non-zero elements.
+For example: [0,1,0,3,12] → [1,3,12,0,0].
+ */
+public class MoveZerosToEnd {
+    public static void main (String [] args){
+        int [] arr = {0,1,0,3,12};
+        int pos = 0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i] = arr[pos];
+                arr[pos]=temp;
+                pos++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
