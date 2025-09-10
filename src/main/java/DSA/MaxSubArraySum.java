64eafb7589fefc5817bfe4cfbaf3efd7824f4404
gourav sharma <gourav.101sharma@gmail.com>
2025-09-10 23:32:48 +0530

    code

---
+++ src/main/java/DSA/MaxSubArraySum.java
@@ -0,0 +1,19 @@
+package DSA;
+/*
+Problem 1: Kadane’s Algorithm (Maximum Subarray Sum)
+
+👉 Problem statement:
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return that sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6
+
+ */
+public class MaxSubArraySum {
+    public static void main (String [] args){
+
+    }
+}
