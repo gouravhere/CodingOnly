f71ea2675b8d6a258f69c3aa3322052cca530cce
gourav sharma <gourav.101sharma@gmail.com>
2025-10-16 21:47:04 +0530

    code

---
+++ src/main/java/DSA/TwoPointers/SortColors.java
@@ -0,0 +1,24 @@
+package DSA.TwoPointers;
+/*
+🧩 Problem: Sort Colors (Dutch National Flag)
+
+LeetCode #75 — Medium
+
+Question:
+You are given an array nums with n objects colored red, white, or blue — represented by 0, 1, and 2.
+Sort them in-place so that objects of the same color are adjacent, in the order red → white → blue (0, 1, 2).
+
+Example:
+
+Input:  nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Constraint: You cannot use a library sort function.
+💡 Hint (don’t code yet):
+Try using three pointers — low, mid, high.
+low → for 0s
+mid → for current element
+high → for 2s
+ */
+public class SortColors {
+}
