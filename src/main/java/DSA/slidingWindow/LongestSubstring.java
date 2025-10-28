951f0a44b49c80c37090b1d86d2f84979f0049fd
gourav sharma <gourav.101sharma@gmail.com>
2025-10-28 22:49:14 +0530

    code

--- src/main/java/DSA/slidingWindow/LongestSubstring.java
+++ src/main/java/DSA/slidingWindow/LongestSubstring.java
@@ -1,4 +1,9 @@
 package DSA.slidingWindow;
+/*
+Find the length of the longest substring without repeating characters.
 
+Example:
+s = "abcabcbb" → answer = 3 (substring "abc")
+ */
 public class LongestSubstring {
 }
