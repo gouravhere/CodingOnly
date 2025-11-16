d42c38926cd09b26f58ac36802a210d9a38bf121
gourav sharma <gourav.101sharma@gmail.com>
2025-11-16 20:29:38 +0530

    code

--- src/main/java/DSA/Revision/RA.java
+++ src/main/java/DSA/Revision/RA.java
@@ -1,9 +1,19 @@
 package DSA.Revision;
 
+import java.util.Arrays;
+
 public class RA {
     public static void main (String [] args){
         int [] arr = {1,2,3,4,5};
-
-        
+        int start = 0;
+        int end = arr.length-1;
+                while(start<end-1){
+                    int temp = arr[start];
+                    arr[start] = arr[end];
+                    arr[end] = temp;
+                    start++;
+                    end--;
+        }
+        System.out.printf(Arrays.toString(arr));
     }
 }
