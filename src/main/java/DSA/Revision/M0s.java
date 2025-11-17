1c7e60a79caa5ffa96ea10391c637236e45b54d8
gourav sharma <gourav.101sharma@gmail.com>
2025-11-18 00:47:54 +0530

    code

--- src/main/java/DSA/Revision/M0s.java
+++ src/main/java/DSA/Revision/M0s.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public class M0s {
     public static void main (String [] args){
         int [] arr ={0,1,0,3,12};
-        int start = 0;
-        int end = arr.length-1;
-        while(arr[start]==0){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+        int pos = 0;
+        for(int i = 0; i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i] = arr[pos];
+                arr[pos] = temp;
+                pos++;
+            }
+
         }
         System.out.printf(Arrays.toString(arr));
     }
