5bbab6c0cb9808fee6b4f8cba8a6ef3144d77c87
gourav sharma <gourav.101sharma@gmail.com>
2025-11-18 21:07:38 +0530

    code

--- src/main/java/DSA/Revision/SSA.java
+++ src/main/java/DSA/Revision/SSA.java
@@ -1,5 +1,13 @@
 package DSA.Revision;
+/*
+Problem: Squares of a Sorted Array (Two Pointers)
+Given a sorted array with negative and positive numbers:
 
+[-5, -3, -2, -1, 4, 6]
+Return the squares in sorted order:
+
+[1,4,9,16,25,36]
+ */
 public class SSA {
     public static void main (String [] args){
 
