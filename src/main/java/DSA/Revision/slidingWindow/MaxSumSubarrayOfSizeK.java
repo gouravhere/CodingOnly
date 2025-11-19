6894237c85df7342932a39550cb0f5d20f83bffc
gourav sharma <gourav.101sharma@gmail.com>
2025-11-19 23:04:03 +0530

    code

--- src/main/java/DSA/Revision/slidingWindow/MaxSumSubarrayOfSizeK.java
+++ src/main/java/DSA/Revision/slidingWindow/MaxSumSubarrayOfSizeK.java
@@ -1,4 +1,15 @@
 package DSA.Revision.slidingWindow;
+/*
+Max Sum Subarray of Size K
 
+Given an array:
+[2, 1, 5, 1, 3, 2]
+and k = 3
+
+Return the maximum sum of any subarray of size 3.
+
+Example:
+[5, 1, 3] → sum = 9
+ */
 public class MaxSumSubarrayOfSizeK {
 }
