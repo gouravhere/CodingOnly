ba741e837e147a7e4cffdc19bcdfd80166b5c652
gourav sharma <gourav.101sharma@gmail.com>
2025-11-19 23:54:53 +0530

    code

--- src/main/java/DSA/Revision/slidingWindow/MaxSumSubarrayOfSizeK.java
+++ src/main/java/DSA/Revision/slidingWindow/MaxSumSubarrayOfSizeK.java
@@ -19,27 +19,23 @@ public class MaxSumSubarrayOfSizeK {
         System.out.println(maxSumWindow(arr, k));
     }
 
-    private static int maxSumWindow(int[] arr, int k) {
+    public static int maxSumWindow(int[] arr, int k) {
+        if (k > arr.length || k <= 0) return 0;
 
-        if(k>arr.length){
-            return 0;
-        }
         int windowSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+        // sum of first k elements
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
 
-        // 2) compute sum of first k elements
-            for (int i=0;i<k;i++){
-                windowSum=+arr[i];
-            }
-            maxSum=windowSum;
-        // 3) slide the window from i = k to arr.length-1
+        int maxSum = windowSum;
+
         // slide the window
         for (int end = k; end < arr.length; end++) {
             windowSum = windowSum - arr[end - k] + arr[end];
             maxSum = Math.max(maxSum, windowSum);
         }
-        //    windowSum = windowSum - arr[i - k] + arr[i]
-        //    update maxSum
+
         return maxSum;
     }
 }
