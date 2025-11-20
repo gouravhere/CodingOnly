4382a894cf6237e8d3077a548854e44dd91479fd
gourav sharma <gourav.101sharma@gmail.com>
2025-11-20 23:04:58 +0530

    code

--- src/main/java/DSA/Revision/slidingWindow/LongestSubString.java
+++ src/main/java/DSA/Revision/slidingWindow/LongestSubString.java
@@ -1,4 +1,27 @@
 package DSA.Revision.slidingWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubString {
+    public static void main(String[] args){
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int maxlen=0;
+        int left =0;
+        Map<Character,Integer> map = new HashMap<>();
+        for (int right =0;right<s.length();right++){
+            char ch = s.charAt(right);
+            if(map.containsKey(ch)){
+                left = Math.max(left,map.get(ch)+1);
+            }
+            map.put(ch,right);
+            maxlen = Math.max(maxlen,right - left +1);
+        }
+
+        return maxlen;
+    }
 }
