95c14ea0df5bd495274cbbcce6ed61a631f79b85
gourav sharma <gourav.101sharma@gmail.com>
2025-11-25 23:43:59 +0530

    code

---
+++ src/main/java/DSA/slidingWindow/LongestSubWithK.java
@@ -0,0 +1,12 @@
+package DSA.slidingWindow;
+
+public class LongestSubWithK {
+    public static void main(String[] args){
+        String s = "abcabcbb";
+        System.out.println(LongestSubstring(s));
+    }
+    private static String LongestSubstring(String s) {
+
+        return s;
+    }
+}
