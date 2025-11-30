608862cf4498d523b921fa46820ffb0e9c11297b
gourav sharma <gourav.101sharma@gmail.com>
2025-11-30 21:08:37 +0530

    code

--- src/main/java/DSA/slidingWindow/LongestSubWithK.java
+++ src/main/java/DSA/slidingWindow/LongestSubWithK.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class LongestSubWithK {
     public static void main(String[] args){
-        String s = "ecebadh";
+        String s = "ecebadhaa";
         int k = 2;
         System.out.println(longestKDistinct(s,k));
     }
