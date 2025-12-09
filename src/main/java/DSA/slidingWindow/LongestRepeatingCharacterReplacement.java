2adc37b73f60f9c4045e7573680550886ad5b96d
gourav sharma <gourav.101sharma@gmail.com>
2025-12-09 22:47:03 +0530

    code

---
+++ src/main/java/DSA/slidingWindow/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,26 @@
+package DSA.slidingWindow;
+/*
+⭐ Problem: Longest Repeating Character Replacement
+🔥 What you are allowed to do:
+
+You’re given:
+
+a string s
+a number k
+
+You may replace at most k characters in the string with any character.
+
+Your goal:
+👉 Find the longest substring that can be turned into a string of the SAME character using at most k replacements.
+
+🎯 Simple Example
+Input:
+s = "AABABBA"
+k = 1
+
+ */
+public class LongestRepeatingCharacterReplacement {
+    public static void main (String [] args){
+
+    }
+}
