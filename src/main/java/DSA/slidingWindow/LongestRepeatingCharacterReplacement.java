4cd931fab6155e09eb737bb3f5c30ac45168afd2
gourav sharma <gourav.101sharma@gmail.com>
2026-01-05 18:47:29 +0530

    code

--- src/main/java/DSA/slidingWindow/LongestRepeatingCharacterReplacement.java
+++ src/main/java/DSA/slidingWindow/LongestRepeatingCharacterReplacement.java
@@ -27,7 +27,7 @@ k = 1
  */
 public class LongestRepeatingCharacterReplacement {
     public static void main (String [] args){
-    String s = "AABBABBAA";
+    String s = "AABBBBAA";
     int k = 2;
         System.out.printf(String.valueOf(characterReplacement(s,k)));
     }
